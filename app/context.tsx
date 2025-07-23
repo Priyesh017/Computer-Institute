57c5c2da057b2152a222f6c29ca90e8b4d1ae390
Mainak908 <ghosh.mainak0401@gmail.com>
2025-07-23 09:31:37 +0530

    fix:known bug fxes

--- app/context.tsx
+++ app/context.tsx
@@ -3,20 +3,9 @@ import { useAuthStore } from "@/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Loader from "@/components/Loader";
 import { useAuth } from "@/hooks/useAuth";
-import { ApiEnd } from "@/helper";
 import Datafetch from "./datafetch";
 
 export const queryClient = new QueryClient();
-
-ApiEnd.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401 && typeof window !== "undefined") {
-      window.location.href = "/noauth";
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default function App({ children }: { children: React.ReactNode }) {
   const { loadingTime } = useAuthStore();
